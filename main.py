fed05fb7ff74a058e8b7f885cd5d53afae0f5a38
ANKIT SINGH <ankit2022svnit@gmail.com>
2025-02-09 17:42:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def trim_audio():
 
 if __name__ == '__main__':
     app.run(
-        host='1.0.0.0',
+        host='0.0.0.0',
         port=5000,
         ssl_context=(
             '/etc/ssl/certs/selfsigned.crt',  # Path to the certificate file
